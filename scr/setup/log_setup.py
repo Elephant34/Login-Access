423faeaec596a29997d724f9c46fbb8a8e2bfa40
Ambrose Hlustik-Smith <a.hlustiksmith@gmail.com>
2019-05-30 16:52:46 +0100

    Started log setup menu

--- scr/setup/log_setup.py
+++ scr/setup/log_setup.py
@@ -1,8 +1,10 @@
 '''
 Sets up the logging files and log paths
 '''
-# Alls for path manipulation
+# Allows for path manipulation
 import pathlib
+# Converts writen numbers into integers
+from scr.utilities.text2int import text2int, Illegal_Word
 
 
 def log_setup():
@@ -10,7 +12,7 @@ def log_setup():
     Gets the users log settings
     '''
 
-    get_log_path()
+    # get_log_path()
     get_log_limmit()
 
 
@@ -33,7 +35,41 @@ def get_log_limmit():
     '''
     log_limmit = "0"
 
-    log_limmit = str(
-        input("Please enter the number of logs you wish to store(0 [Unlimited])"))
+    valid = False
+
+    while not valid:
+        log_limmit = str(
+            input("Please enter the number of logs you wish to store(0 [Unlimited])"))
+
+        valid = verify_log_limmit(log_limmit)
 
     return log_limmit
+
+
+def verify_log_limmit(log_limmit):
+    '''
+    Verifies the limmit to the number of log files
+    '''
+    validity = False
+
+    # Tests if the input is a number
+    try:
+        # The value succeeded therefore continue
+        log_limmit = int(log_limmit)
+        validity = True
+    except ValueError:
+        # Not a direct number :(
+
+        try:
+            # Attempts to convert the words into a number
+            log_limmit = text2int(log_limmit)
+            validity = True
+        except Illegal_Word:
+            # text cannot be converted into a string
+            pass
+
+    # Ensures the number isn't negetive
+    if log_limmit < 0:
+        validity = False
+
+    return validity
---
+++ scr/utilities/text2int.py
@@ -0,0 +1,61 @@
+'''
+Converts a text writen number into an integer
+'''
+
+
+class Illegal_Word(Exception):  # pylint: disable=invalid-name
+    '''
+    Custom exception for invalid words
+    '''
+
+    def __init__(self, message):  # pylint: disable=useless-super-delegation
+
+        # Calls the Exceptions initialization method
+        super().__init__(message)
+
+
+def text2int(textnum, numwords=None):
+    '''
+    Converts a writen number to integer
+
+    Credit to stackoverflow user: https://stackoverflow.com/users/44743/recursive
+    '''
+    if not numwords:
+        numwords = {}
+        units = [
+            "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+            "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+            "sixteen", "seventeen", "eighteen", "nineteen",
+        ]
+
+        tens = ["", "", "twenty", "thirty", "forty",
+                "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+        scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+        numwords["and"] = (1, 0)
+        for idx, word in enumerate(units):
+            numwords[word] = (1, idx)
+        for idx, word in enumerate(tens):
+            numwords[word] = (1, idx * 10)
+        for idx, word in enumerate(scales):
+            numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+    current = result = 0
+    for word in textnum.split():
+        if word not in numwords:
+            raise Illegal_Word
+
+        scale, increment = numwords[word]
+        current = current * scale + increment
+        if scale > 100:
+            result += current
+            current = 0
+
+    return result + current
+
+
+# try:
+#    print(text2int(str(input(""))))
+# except Illegal_Word:
+#    pass
